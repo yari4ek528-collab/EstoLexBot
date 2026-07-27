ea8af04cdfb075fb0bacac3ee64695f4876af59f
yari4ek528-collab <yari4ek528@gmail.com>
2026-07-27 05:53:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -103,6 +103,7 @@ app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("about", about))
 app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("words", words))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, translate))
 
 print("EstoLex запущен...")
